60400b933f88ee9148dcaf44cee757a50256abaa
tuhinhossain123 <csetuhin55@gmail.com>
2024-10-29 18:00:25 +0600

    custom validation add

--- src/app/modules/student.model.ts
+++ src/app/modules/student.model.ts
@@ -7,74 +7,96 @@ import {
 } from './students/student.interface';
 
 const userNameSchema = new Schema<UserName>({
-  firstName: { 
-    type: String, 
-    required: [true, 'First name is required.'] 
+  firstName: {
+    type: String,
+    required: [true, 'First name is required.'],
+    trim: true,
+    maxlength: [20, 'First Name can not be mor then 20 cheracters'],
+    validate: {
+      validator: function (value: string) {
+        const firstNameStr = value.charAt(0).toUpperCase() + value.slice(1);
+        return firstNameStr === value;
+      },
+      message: '{VALUE} is not capitalize formate',
+    },
   },
-  middleName: { 
-    type: String 
+  middleName: {
+    type: String,
+    trim: true,
   },
-  lastName: { 
-    type: String, 
-    required: [true, 'Last name is required.'] 
+  lastName: {
+    type: String,
+    required: [true, 'Last name is required.'],
+    trim: true,
   },
 });
 
 const guardianSchema = new Schema<Guardian>({
-  father: { 
-    type: String, 
-    required: [true, 'Father name is required.'] 
+  father: {
+    type: String,
+    required: [true, 'Father name is required.'],
+    trim: true,
   },
-  fatherOccupation: { 
-    type: String, 
-    required: [true, 'Father’s occupation is required.'] 
+  fatherOccuption: {
+    type: String,
+    required: [true, 'Father’s occupation is required.'],
+    trim: true,
   },
-  fatherContact: { 
-    type: String, 
-    required: [true, 'Father’s contact number is required.'] 
+  fatherContact: {
+    type: String,
+    required: [true, 'Father’s contact number is required.'],
+    trim: true,
   },
-  mother: { 
-    type: String, 
-    required: [true, 'Mother name is required.'] 
+  mother: {
+    type: String,
+    required: [true, 'Mother name is required.'],
+    trim: true,
   },
-  motherOccupation: { 
-    type: String, 
-    required: [true, 'Mother’s occupation is required.'] 
+  motherOccuption: {
+    type: String,
+    required: [true, 'Mother’s occupation is required.'],
+    trim: true,
   },
-  motherContact: { 
-    type: String, 
-    required: [true, 'Mother’s contact number is required.'] 
+  motherContact: {
+    type: String,
+    required: [true, 'Mother’s contact number is required.'],
+    trim: true,
   },
 });
 
 const localGuardianSchema = new Schema<LocalGuardian>({
-  name: { 
-    type: String, 
-    required: [true, 'Local guardian name is required.'] 
+  name: {
+    type: String,
+    required: [true, 'Local guardian name is required.'],
+    trim: true,
   },
-  occupation: { 
-    type: String, 
-    required: [true, 'Local guardian’s occupation is required.'] 
+  occupation: {
+    type: String,
+    required: [true, 'Local guardian’s occupation is required.'],
+    trim: true,
   },
-  contactNo: { 
-    type: String, 
-    required: [true, 'Local guardian’s contact number is required.'] 
+  contactNo: {
+    type: String,
+    required: [true, 'Local guardian’s contact number is required.'],
+    trim: true,
   },
-  address: { 
-    type: String, 
-    required: [true, 'Local guardian’s address is required.'] 
+  address: {
+    type: String,
+    required: [true, 'Local guardian’s address is required.'],
+    trim: true,
   },
 });
 
 const studentSchema = new Schema<Student>({
-  id: { 
-    type: String, 
-    required: [true, 'Student ID is required.'], 
-    unique: true 
+  id: {
+    type: String,
+    required: [true, 'Student ID is required.'],
+    unique: true,
+    trim: true,
   },
-  name: { 
-    type: userNameSchema, 
-    required: [true, 'Student name is required.'] 
+  name: {
+    type: userNameSchema,
+    required: [true, 'Student name is required.'],
   },
   gender: {
     type: String,
@@ -83,47 +105,58 @@ const studentSchema = new Schema<Student>({
       message: '{VALUE} is not a valid gender option.',
     },
     required: [true, 'Gender is required.'],
+    trim: true,
   },
-  dateOfBirth: { 
-    type: String 
+  dateOfBirth: {
+    type: String,
+    trim: true,
   },
-  contactNo: { 
-    type: String, 
-    required: [true, 'Student contact number is required.'] 
+  contactNo: {
+    type: String,
+    required: [true, 'Student contact number is required.'],
+    trim: true,
   },
-  email: { 
-    type: String, 
-    required: [true, 'Email address is required.'], 
-    unique: true 
+  email: {
+    type: String,
+    required: [true, 'Email address is required.'],
+    unique: true,
+    trim: true,
   },
-  emergencyContact: { 
-    type: String 
+  emargencyContact: {
+    type: String,
+    trim: true,
   },
   bloodGroup: {
     type: String,
+    trim: true,
     enum: {
       values: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
       message: '{VALUE} is not a valid blood group.',
     },
   },
-  presentAddress: { 
-    type: String, 
-    required: [true, 'Present address is required.'] 
+  presetAddress: {
+    type: String,
+    required: [true, 'Present address is required.'],
+    trim: true,
   },
-  permanentAddress: { 
-    type: String, 
-    required: [true, 'Permanent address is required.'] 
+  permanentAddress: {
+    type: String,
+    required: [true, 'Permanent address is required.'],
+    trim: true,
   },
-  guardian: { 
-    type: guardianSchema, 
-    required: [true, 'Guardian details are required.'] 
+  guardian: {
+    type: guardianSchema,
+    required: [true, 'Guardian details are required.'],
+    trim: true,
   },
-  localGuardian: { 
-    type: localGuardianSchema, 
-    required: [true, 'Local guardian details are required.'] 
+  localGuardian: {
+    type: localGuardianSchema,
+    required: [true, 'Local guardian details are required.'],
+    trim: true,
   },
-  photoUrl: { 
-    type: String 
+  photoUrl: {
+    type: String,
+    trim: true,
   },
   isActive: {
     type: String,
@@ -132,6 +165,7 @@ const studentSchema = new Schema<Student>({
       message: '{VALUE} is not a valid status option.',
     },
     default: 'active',
+    trim: true,
   },
 });
 
--- src/app/modules/students/student.controller.ts
+++ src/app/modules/students/student.controller.ts
@@ -11,7 +11,11 @@ const crearteStudent = async (req: Request, res: Response) => {
       data: result,
     });
   } catch (error) {
-    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: 'Something went wrong',
+      error: error,
+    });
   }
 };
 
